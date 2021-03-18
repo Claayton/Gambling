09eeadff81fe73b34be3d9c7f3400c2810e6da61
Clayton <claaytongarcia@gmail.com>
2021-03-17 23:56:09 -0300

    aiai

--- inicio.py
+++ inicio.py
@@ -4,20 +4,29 @@ from PySimpleGUI import PySimpleGUI as sg
 def janela_choice():
     sg.theme('Reddit')
     layout = [
-        [sg.Button(f'{"JOKENPÔ":^53}', key='JOKENPO')],
-        [sg.Button(f'{"PAR OU ÍMPAR":^49}', key='PAROUIMPAR')],     
+        [sg.Button(f'{"JOKENPÔ":^53}')],
+        [sg.Button(f'{"PAR OU ÍMPAR":^49}')],     
     ]
     return sg.Window('JOGOS', layout=layout, finalize=True)
 def janela_jokenpo():
     sg.theme('Reddit')
     layout = [
-        [sg.Button(f'{"PEDRA":^48}', key='PEDRA')],
-        [sg.Button(f'{"PAPEL":^49}', key='PAPEL')],
-        [sg.Button(f'{"TESOURA":^46}', key='TESOURA')],        
+        [sg.Button(f'{"PEDRA":^48}')],
+        [sg.Button(f'{"PAPEL":^49}')],
+        [sg.Button(f'{"TESOURA":^46}')],        
         [sg.Output(size=(33, 6))]
-        [sg.Button(f'{"Voltar":^49}', key='voltar')], 
+        [sg.Button(f'{"Voltar":^49}',)], 
     ]
     return sg.Window('JOKENPO', layout=layout, finalize=True)
+def janela_parouimpar():
+    sg.theme('Reddit')
+    layout = [
+        [sg.Checkbox('PAR', key='par'), sg.Checkbox('Impar', key='impar')],
+        [sg]
+        [sg.Button(f'{"JOKENPÔ":^53}')],
+        [sg.Button(f'{"PAR OU ÍMPAR":^49}')],     
+    ]
+    return sg.Window('JOGOS', layout=layout, finalize=True)
 
 # Criar as janelas iniciais
 janela01, janela02 = janela_choice(), None
@@ -27,7 +36,7 @@ while True:
     window, event, values = sg.read_all_windows()
     if window == janela01 and event == sg.WIN_CLOSED:
         break
-    if window == janela01 and event == 'PAROUIMPAR':
+    if window == janela01 and event == 'JOKENPÔ':
         janela02 = janela_jokenpo()
         janela01.hide()
     if window == janela02 and event == 'Voltar':
