f14ceb968acf08e28546a91640d963de2ddfbf97
Clayton <claaytongarcia@gmail.com>
2021-03-18 13:44:55 -0300

    18-03
    adicionado modo jokenpo, mas projeto ainda esta em desenvolvimento

--- inicio.py
+++ inicio.py
@@ -1,29 +1,10 @@
 from PySimpleGUI import PySimpleGUI as sg
-
+from jokenpo import jokenpo
 # Criar janelas e estilos (layout)
 def janela_choice():
     sg.theme('Reddit')
     layout = [
-        [sg.Button(f'{"JOKENPÔ":^53}')],
-        [sg.Button(f'{"PAR OU ÍMPAR":^49}')],     
-    ]
-    return sg.Window('JOGOS', layout=layout, finalize=True)
-def janela_jokenpo():
-    sg.theme('Reddit')
-    layout = [
-        [sg.Button(f'{"PEDRA":^48}')],
-        [sg.Button(f'{"PAPEL":^49}')],
-        [sg.Button(f'{"TESOURA":^46}')],        
-        [sg.Output(size=(33, 6))]
-        [sg.Button(f'{"Voltar":^49}',)], 
-    ]
-    return sg.Window('JOKENPO', layout=layout, finalize=True)
-def janela_parouimpar():
-    sg.theme('Reddit')
-    layout = [
-        [sg.Checkbox('PAR', key='par'), sg.Checkbox('Impar', key='impar')],
-        [sg]
-        [sg.Button(f'{"JOKENPÔ":^53}')],
+        [sg.Button(f'{"JOKENPÔ":^53}', key='JOKENPO')],
         [sg.Button(f'{"PAR OU ÍMPAR":^49}')],     
     ]
     return sg.Window('JOGOS', layout=layout, finalize=True)
@@ -33,13 +14,13 @@ janela01, janela02 = janela_choice(), None
 
 # Criar loop para leitura de eventos 
 while True:
-    window, event, values = sg.read_all_windows()
+    window, event, values = sg.read_all_windows() 
     if window == janela01 and event == sg.WIN_CLOSED:
         break
-    if window == janela01 and event == 'JOKENPÔ':
-        janela02 = janela_jokenpo()
+    if window == janela01 and event == 'JOKENPO':
         janela01.hide()
-    if window == janela02 and event == 'Voltar':
-        jaela02.hide()
+        janlea02 = jokenpo()
+    if window == janela02 and event == sg.WIN_CLOSED:
+        janela02.hide()
         janela01.un_hide()
         
