a089003853248565d1d385be106f819598390a76
Clayton <claaytongarcia@gmail.com>
2021-03-31 11:44:47 -0300

    31-03

--- inicio.py
+++ inicio.py
@@ -1,6 +1,7 @@
 from PySimpleGUI import PySimpleGUI as sg
 from janelas import jokenpo
 from janelas import parouimpar
+from janelas import calculadora
 
 # Janela inicio
 def janela01():
@@ -8,12 +9,13 @@ def janela01():
     sg.theme('Reddit')
     layout = [
         [sg.Button(f'{"JOKENPÔ":^53}', key='JOKENPO')],
-        [sg.Button(f'{"PAR OU ÍMPAR":^49}', key='PAROUIMPAR')],     
+        [sg.Button(f'{"PAR OU ÍMPAR":^49}', key='PAROUIMPAR')],   
+        [sg.Button(f'{"CALCULADORA":^48}', key='CALCULADORA')],  
     ]
-    return sg.Window('JOGOS', layout=layout, finalize=True)
+    return sg.Window('Mini Programas', layout=layout, finalize=True)
 
 # Criar as janelas iniciais
-janela01, janela02, janela03 = janela01(), None, None
+janela01, janela02, janela03, janela04 = janela01(), None, None, None
 
 # Criar loop para leitura de eventos 
 while True:
@@ -27,4 +29,8 @@ while True:
     if window == janela01 and  event == 'PAROUIMPAR':
         janela03 = parouimpar()
     if window == janela03 and event == sg.WIN_CLOSED:
+        janela01.read()
+    if window == janela01 and  event == 'CALCULADORA':
+        janela04 = calculadora()
+    if window == janela04 and event == sg.WIN_CLOSED:
         janela01.read()
