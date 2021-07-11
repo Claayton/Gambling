e66f2871f4dca9365dff6197253ad08b85e65f50
Clayton Garcia <claaytongarcia@gmail.com>
2021-07-11 20:24:42 -0300

    Realocaçao da funçao de nomo no main do programa

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def janela_inicio():
 
     sg.theme('DarkBlue14')
     layout = [
+        [sg.Text(size=(WIN_W,1), background_color = '#272828')],
+        [sg.Text('Digite aqui seu nome: ', size=(18, 1), font=("Helvetica", 20), background_color = '#272828', text_color='white', key='nome')
+        ,sg.Input(size=(20, 1), font=("Helvetica", 20), background_color = '#2f3030', text_color='white')],
+        [sg.Button('Entrar', size=(80, 3), key='Enter')],
         [sg.Canvas(background_color='black', size=(650, 10), pad=None)],
         [sg.Text(' \n           TESTE SUA SORTE\n ', size=(26, 3), font=('mood', 30), background_color = '#272828', text_color='white')],    
         [sg.Canvas(background_color='black', size=(650, 10), pad=None)],
@@ -40,27 +44,25 @@ def janela_inicio():
 # -------------------------------------------------------------------------
 # Criar as janelas iniciais
 
-janela01, janela02, janela03, janela04, janela05 = recorde(), None, None, None, None
+janela01, janela02, janela03, janela04 = janela_inicio(), None, None, None
 
 # -------------------------------------------------------------------------
 # Criar loop para leitura de eventos 
 
 while True:
     window, event, valores = sg.read_all_windows(timeout=1)
-    if window == janela01 and event in (sg.WIN_CLOSED, 'Cancel', 'Enter'):
-        janela02 = janela_inicio()
-    if window == janela02 and event in (sg.WIN_CLOSED, 'Cancel'):
+    if window == janela01 and event in (sg.WIN_CLOSED, 'Cancel'):
         break
-    if window == janela02 and event == 'JOKENPO':
-        janela02.hide()
-        janela03 = jokenpo()
-        janela02.UnHide()
-    if window == janela02 and  event == 'PAROUIMPAR':
-        janela02.hide()
-        janela04 = parouimpar()
-        janela02.UnHide()
-    if window == janela02 and event == 'DADO':
-        janela02.hide()
-        janela05 = dado()
-        janela02.UnHide()
+    if window == janela01 and event == 'JOKENPO':
+        janela01.hide()
+        janela02 = jokenpo()
+        janela01.UnHide()
+    if window == janela01 and  event == 'PAROUIMPAR':
+        janela01.hide()
+        janela03 = parouimpar()
+        janela01.UnHide()
+    if window == janela01 and event == 'DADO':
+        janela01.hide()
+        janela04 = dado()
+        janela01.UnHide()
 window.close()
