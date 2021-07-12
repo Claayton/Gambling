944536d60bf10ec468ee17ded06a996c3e9b120a
Clayton Garcia <claaytongarcia@gmail.com>
2021-07-12 10:02:53 -0300

    Atualizado layout da janela principal

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
+import buttons
 from PySimpleGUI import PySimpleGUI as sg
 from jokenpo import jokenpo
 from dado import dado
 from parouimpar import parouimpar
-from recorde import recorde
-import buttons
 
 # -------------------------------------------------------------------------
 # Janela inicial
 
 
 def janela_inicio():
-    WIN_W = 80
-    WIN_H = 25
+    WIN_W = 650
+    WIN_H = 450
+    bgcolor = '#272828'
+    ccolor = 'black'
 
     sg.theme('DarkBlue14')
     layout = [
-        [sg.Text(size=(WIN_W,1), background_color = '#272828')],
-        [sg.Text('Digite aqui seu nome: ', size=(18, 1), font=("Helvetica", 20), background_color = '#272828', text_color='white', key='nome')
-        ,sg.Input(size=(20, 1), font=("Helvetica", 20), background_color = '#2f3030', text_color='white')],
-        [sg.Button('Entrar', size=(80, 3), key='Enter')],
-        [sg.Canvas(background_color='black', size=(650, 10), pad=None)],
-        [sg.Text(' \n           TESTE SUA SORTE\n ', size=(26, 3), font=('mood', 30), background_color = '#272828', text_color='white')],    
-        [sg.Canvas(background_color='black', size=(650, 10), pad=None)],
-        [sg.Canvas(background_color='#272828', size=(30, 150), pad=None),
+        [sg.Text('Digite aqui seu nome:', size=(18, 1), font=("Helvetica", 15), background_color = bgcolor, text_color='white', key='nome'),
+        sg.Input(size=(17, 1), font=("Helvetica", 20), background_color = '#2f3030', text_color='white'),
+        sg.Text(' ☒', size=(2, 1), font=("Helvetica", 22), background_color = bgcolor, text_color='red', key='gravado'),
+        sg.Button('Gravar', size=(16, 1), key='Gravar')],       
+        [sg.Canvas(background_color=ccolor, size=(650, 10), pad=None)],
+        [sg.Text('   TESTE SUA SORTE', size=(26, 1), font=('Dyuthi', 50), background_color = bgcolor, text_color='white')],    
+        [sg.Canvas(background_color=ccolor, size=(650, 10), pad=None)],
+        [sg.Text('       JOKENPÔ           PAR OU ÍMPAR            DADO          ', size=(50, 1), font=('Dyuthi', 20), background_color = bgcolor, text_color='white')],
+        [sg.Canvas(background_color=ccolor, size=(650, 10), pad=None)],    
+        [sg.Canvas(background_color=bgcolor, size=(20, 150), pad=None),
         sg.Button('', image_data=buttons.button_jokenpo, key='JOKENPO', button_color=(sg.theme_background_color('#4f4f4f'), sg.theme_background_color('#4f4f4f')), border_width=0.5),
-        sg.Canvas(background_color='#272828', size=(30, 150), pad=None), 
+        sg.Canvas(background_color=bgcolor, size=(30, 150), pad=None), 
         sg.Button('', image_data=buttons.button_poui64, key='PAROUIMPAR', button_color=(sg.theme_background_color('#4f4f4f'), sg.theme_background_color('#4f4f4f')), border_width=0.5),
-        sg.Canvas(background_color='#272828', size=(30, 150), pad=None), 
+        sg.Canvas(background_color=bgcolor, size=(30, 150), pad=None), 
         sg.Button('', image_data=buttons.button_dado64, key='DADO', button_color=(sg.theme_background_color('#4f4f4f'), sg.theme_background_color('#4f4f4f')), border_width=0.5),
-        sg.Canvas(background_color='#272828', size=(30, 150), pad=None)],
-        [sg.Canvas(background_color='black', size=(650, 10), pad=None)]
-    ]
+        sg.Canvas(background_color=bgcolor, size=(20, 150), pad=None)],
+        [sg.Canvas(background_color=ccolor, size=(650, 10), pad=None)]
+        ]
 
-    return sg.Window('Jogramas',
+    return sg.Window('Gambling',
+    size=(WIN_W, WIN_H),
     layout=layout,
     resizable=True,
     return_keyboard_events=True,
     finalize=True,
+    titlebar_icon= 'imagens/icon_main.png',
     icon= 'imagens/icon_main.png',
-    background_color = '#272828')
+    background_color = bgcolor)
 
 
 # -------------------------------------------------------------------------
@@ -53,6 +58,8 @@ while True:
     window, event, valores = sg.read_all_windows(timeout=1)
     if window == janela01 and event in (sg.WIN_CLOSED, 'Cancel'):
         break
+    if window == janela01 and event == 'Gravar':
+        window['gravado'].update(' ☑', text_color='green', font=("Helvetica", 22))
     if window == janela01 and event == 'JOKENPO':
         janela01.hide()
         janela02 = jokenpo()
