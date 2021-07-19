8bd2e01bc32d9ed5ea4415e782b0ce9e43faa2a9
Clayton Garcia <claaytongarcia@gmail.com>
2021-07-19 12:15:16 -0300

    Atualizadas imagens de Jokenpo

--- games/jokenpo.py
+++ games/jokenpo.py
@@ -8,19 +8,28 @@ def jokenpo():
     pcescolha = ''
     result = ''
     possibilidades = ['PEDRA', 'PAPEL', 'TESOURA']
+    placar_pc = 0
+    placar_player = 0
+    nome = ler_nome_do_ultimo_player()
     WIN_W = 650
     WIN_H = 450
     bg_color = '#4f4f4f'
 
+
     # layout
     def jokenpo_inicio():
-        
         if ler_nome_do_ultimo_player() != 'Default':
             nome = ler_nome_do_ultimo_player()
         else:
             nome = ''
-        
+
+
         sg.theme('DarkBlue14')
+
+        global placar_pc, placar_player
+        placar_pc = 0
+        placar_player = 0
+
         layout = [
             [sg.Canvas(background_color=bg_color, size=(650, 20), pad=None)], 
             [sg.Canvas(background_color='#272828', size=(650, 10), pad=None)], 
@@ -38,8 +47,8 @@ def jokenpo():
             sg.Canvas(background_color=bg_color, size=(60, 150), pad=None)],
             [sg.Canvas(background_color='#272828', size=(650, 10), pad=None)],
             [sg.Canvas(background_color=bg_color, size=(650, 10), pad=None)],
-            [sg.Text(f'Placar PC: {2}', size= (24, 1),font=('Dyuthi', 16), background_color = bg_color, justification='left'),
-            sg.Text(f'Placar do Player: {2}', size= (24, 1), font=('Dyuthi', 16), background_color = bg_color, justification='left')]
+            [sg.Text(f'Placar PC: {placar_pc}', size= (24, 1),font=('Dyuthi', 16), background_color = bg_color, justification='left', key='placar_pc'),
+            sg.Text(f'Placar do Player: {placar_player}', size= (24, 1), font=('Dyuthi', 16), background_color = bg_color, justification='left', key='placar_player')]
         ]
 
         # janela
@@ -54,7 +63,7 @@ def jokenpo():
 
     def jogar():
         while True:
-            for i in range(20000):
+            for i in range(19000):
                 sg.popup_animated(
                     gif,
                     location=(350, 190),
@@ -65,13 +74,20 @@ def jokenpo():
         return sg.popup_animated(None)
 
 
-    def mostrar_resultado():
+    def mostrar_resultado(nome='Player'):
+        if nome == 'Default' or nome == '':
+            nome = 'Player'
+
         sg.theme('DarkBlue14')
         layout = [
+            [sg.Canvas(background_color=bg_color, size=(650, 10), pad=None)],
+            [sg.Text(f'{result}', size=(26, 1), font=('Dyuthi', 50), background_color = '#4F4F4F', text_color='white', justification='center')],
             [sg.Image(png, background_color = '#4F4F4F')],
-            [sg.Canvas(background_color=bg_color, size=(650, 20), pad=None)], 
-            [sg.Button(f'{"Cancel":^}', key='Cancel', size=(41,5)),
-            sg.Button(f'{"JOGAR NOVAMENTE":^}', key='JOGAR', size=(41,5))]
+            [sg.Canvas(background_color=bg_color, size=(650, 20), pad=None)],
+            [sg.Text(f'PC', size= (13, 2),font=('Dyuthi', 30), background_color = bg_color, justification='center'),
+            sg.Text(f'{nome}', size= (13, 2),font=('Dyuthi', 30), background_color = bg_color, justification='center')],
+            [sg.Button(f'{"Cancel":^}', key='Cancel', size=(41,3)),
+            sg.Button(f'{"JOGAR NOVAMENTE":^}', key='JOGAR', size=(41,3))]
             ]
 
         return sg.Window('Jokenpo',
@@ -93,7 +109,6 @@ def jokenpo():
         window, eventos, values = sg.read_all_windows(timeout=1)
         if window == janela01 and eventos in (sg.WIN_CLOSED, 'Cancel'):
             break
-        
         if window == janela01 and eventos == 'PEDRA' :
             p1escolha = 'PEDRA'
         elif window == janela01 and eventos == 'PAPEL':
@@ -119,35 +134,42 @@ def jokenpo():
         elif pcescolha == 'PEDRA' and p1escolha == 'TESOURA':
             gif = 'imagens/jokenpo/jokenpo_gif/pedra_tesoura.gif'
             png = 'imagens/jokenpo/jokenpo_png/pedra_tesoura_result.png'
-            result = 'Venceu'
+            result = 'Perdeu'
         elif pcescolha == 'PAPEL' and p1escolha == 'PEDRA':
             gif = 'imagens/jokenpo/jokenpo_gif/papel_pedra.gif'
             png = 'imagens/jokenpo/jokenpo_png/papel_pedra_result.png'
-            result = 'Venceu'
+            result = 'Perdeu'
         elif pcescolha == 'TESOURA' and p1escolha == 'PAPEL':
             gif = 'imagens/jokenpo/jokenpo_gif/tesoura_papel.gif'
             png = 'imagens/jokenpo/jokenpo_png/tesoura_papel_result.png'
-            result = 'Venceu'
+            result = 'Perdeu'
 
         elif pcescolha == 'PEDRA' and p1escolha == 'PAPEL':
             gif = 'imagens/jokenpo/jokenpo_gif/pedra_papel.gif'
             png = 'imagens/jokenpo/jokenpo_png/pedra_papel_result.png'
-            result = 'Perdeu'
+            result = 'Venceu'
         elif pcescolha == 'PAPEL' and p1escolha == 'TESOURA':
             gif = 'imagens/jokenpo/jokenpo_gif/papel_tesoura.gif'
             png = 'imagens/jokenpo/jokenpo_png/papel_tesoura_result.png'
-            result = 'Perdeu'
+            result = 'Venceu'
         elif pcescolha == 'TESOURA' and p1escolha == 'PEDRA':
             gif = 'imagens/jokenpo/jokenpo_gif/tesoura_pedra.gif'
             png = 'imagens/jokenpo/jokenpo_png/tesoura_pedra_result.png'
-            result = 'Perdeu'
+            result = 'Venceu'
 
         if window == janela01 and eventos in ('PEDRA', 'PAPEL', 'TESOURA'):
             janela01.Hide()
-            janela02 = mostrar_resultado()
+            janela02 = mostrar_resultado(nome)
             janela02.Hide()
             jogar()
             janela02.UnHide()
+            if result == 'Perdeu':
+                placar_pc += 1
+                window['placar_pc'].update(f'Placar PC: {placar_pc}')
+            elif result == 'Venceu':
+                placar_player += 1
+                window['placar_player'].update(f'Placar Player: {placar_player}')
+
         if window == janela02 and eventos == 'JOGAR':
             janela02.Hide()
             janela01.UnHide()
